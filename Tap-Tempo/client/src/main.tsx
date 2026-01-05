4dc49360ca92f9ea2f7bc8b8572201a34bea0152
djba728 <djba728@yahoo.co.jp>
2026-01-05 16:47:08 +0900

    Import index.css in main.tsx

--- Tap-Tempo/client/src/main.tsx
+++ Tap-Tempo/client/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { Router } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <Router hook={useHashLocation}>
